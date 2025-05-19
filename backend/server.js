80e95f9d4b3697ae780e52d3fde17c15bad342ad
Devi Tanuja Veera <166426256+tanu-119@users.noreply.github.com>
2025-05-19 10:24:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const recommendationRoutes = require("./routes/recommendations");
 const app = express();
 
 app.use(cors({
-  origin: ["https://job-search-frontend-alqe.onrender.com"],
+  origin:"*",
   methods: ["GET", "POST", "PUT"],
   credentials: true
 }));
